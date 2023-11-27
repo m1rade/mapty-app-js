440ee928bba5e1c5d979fee1c01575f7a552131a
m1rade <forx98@gmail.com>
2023-11-27 18:51:03 +0300

    display map feature

--- script.js
+++ script.js
@@ -10,3 +10,30 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+// Get user geolocation
+if (navigator.geolocation)
+    navigator.geolocation.getCurrentPosition(
+        function (position) {
+            const { latitude, longitude } = position.coords;
+            console.log(latitude, longitude);
+            console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+            // Display map on current location
+            const map = L.map('map').setView([latitude, longitude], 15);
+
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+            }).addTo(map);
+
+            map.on('click', function(mapEvent) {
+                console.log(mapEvent);
+                const {lat, lng} = mapEvent.latlng;
+
+                L.marker([lat, lng]).addTo(map).bindPopup(L.popup({}));
+            });
+        },
+        function () {
+            console.error('Could not get your position');
+        }
+    );
